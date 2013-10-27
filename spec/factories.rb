61f4bd18d8f351a30402feb62279b84e921faf76
HC008 <h.wk.cheng@gmail.com>
2013-10-26 19:00:51 -1000

    Gravtar image, and follow tutorial chapter 7

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Hansen Cheng"
+    email    "chengh@hawaii.edu"
+    password "moLiCha@1989"
+    password_confirmation "moLiCha@1989"
+  end
+end
