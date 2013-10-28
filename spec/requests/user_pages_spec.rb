7cf0a0388e655d07bb6962ba15e8971507ed43aa
HC008 <h.wk.cheng@gmail.com>
2013-10-27 21:37:19 -1000

    User can signup now.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,7 +17,32 @@ describe "User pages" do
 
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
+    
   end
   
-  
+  describe "signup" do
+
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Hansen Cheng"
+        fill_in "Email",        with: "chengh@hawaii.edu"
+        fill_in "Password",     with: "moLiCha@1989"
+        fill_in "Confirmation", with: "moLiCha@1989"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
 end
